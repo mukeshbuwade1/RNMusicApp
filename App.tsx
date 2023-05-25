84939d6ccfc491a94d575e850132fea2615b0b79
Mukesh Buwade <mukesh@newrise.in>
2023-05-25 13:31:04 +0530

    fist

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen';
+import MusicList from './src/screens/MusicList';
+
+
+// links 
+// https://github1s.com/devandres-tech/RN-Spotify-Clone-Redesigned/blob/main/screens/TrackPlayer.tsx
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+  return (
+   <>
+   <MusicList/>
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/screens/MusicList.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MusicList = () => {
+  return (
+    <View>
+      <Text>MusicList</Text>
+    </View>
+  )
+}
+
+export default MusicList
+
+const styles = StyleSheet.create({})
